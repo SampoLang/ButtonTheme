d81dfa089aa12f979791183ccb78d30e86baeec8
SampoLang <bgu444@myy.haaga-helia.fi>
2022-10-03 13:48:55 +0300

    exercise done

---
+++ src/HeaderButton.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ThemeContext from './ThemeContext';
+
+function HeaderButton() {
+  const theme = React.useContext(ThemeContext);
+  return (
+    <div>
+      <button style={theme}>Press me</button>
+    </div>
+  );
+}
+
+export default HeaderButton;
